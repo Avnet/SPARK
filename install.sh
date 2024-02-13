f419ac8de454a36cd8e4e53fc24040d332954f87
ljkeller <kellerjlucas@gmail.com>
2024-02-13 21:39:27 +0000

    Refining release install
    Bundling spark exe, touching up install script.

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ INSTALL_DIR=/opt/spark
 mkdir -p "${INSTALL_DIR}"
 
 cp -r ./app/exe/* "${INSTALL_DIR}"
+cat ./app/weston_conf/weston.ini.append >> /etc/xdg/weston/weston.ini
+
+echo "SPARK installed successfully. Restarting Weston for changes to take effect."
+
+systemctl restart weston@root
