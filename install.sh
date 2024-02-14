29e820043c5e3adae41f73cd0f2118e393a2ab75
ljkeller <kellerjlucas@gmail.com>
2024-02-14 16:01:13 +0000

    Bugfix on tvm install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ INSTALL_DIR=/opt/spark
 mkdir -p "${INSTALL_DIR}"
 
 cp -r ./app/exe/* "${INSTALL_DIR}"
-cp ./app/runtime_deps/libtvm_runtime.so /usr/lib64/
+cp ./runtime_deps/libtvm_runtime.so /usr/lib64/
 cat ./app/weston_conf/weston.ini.append >> /etc/xdg/weston/weston.ini
 
 echo "SPARK installed successfully. Restarting Weston for changes to take effect."
