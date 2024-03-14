a95cbe9a1cb8cb1d08eebdacabcbb044b5965c08
ljkeller <kellerjlucas@gmail.com>
2024-03-14 00:29:14 +0000

    Exception handling & exponential backoff

--- app/exe/send_iot_connect.py
+++ app/exe/send_iot_connect.py
@@ -3,12 +3,16 @@
 import json
 import socket
 import sys
+import signal
 import time
 import random
 from datetime import datetime
 from typing import Dict, Any, List
 
 from iotconnect import IoTConnectSDK
+
+class SignalException(Exception):
+    """Custom exception to exit gracefully"""
 
 class IoTConnectClient:
     """Send SPARK data to IoTConnect platform using IoTConnect SDK."""
@@ -34,6 +38,8 @@ class IoTConnectClient:
         }
         self.sdk = None
         self.device_list = []
+        self.setup_exit_handler()
+        self.run_continuously = True
 
     def load_config(self, config_paths: List[str]) -> None:
         """Dependency inject client configuration"""
@@ -49,7 +55,9 @@ class IoTConnectClient:
 
     def connect_spark_socket(self, retry_interval: int = 5) -> socket.socket:
         """Attempt connection (continuously) to SPARK producer socket"""
-        while True:
+        max_retry_backoff_s = 300
+        retry_backoff_s = 5
+        while self.run_continuously:
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.connect((self.config['spark_socket_ipv4'], self.config['spark_socket_port']))
@@ -57,10 +65,36 @@ class IoTConnectClient:
                 return sock
             except ConnectionRefusedError:
                 print("Connection refused, retrying...")
+            except SignalException:
+                sys.exit(0)
             except Exception as e:
                 print(f"An error occurred: {e}. Retrying...")
             finally:
-                time.sleep(retry_interval)
+                retry_backoff_s = min(max_retry_backoff_s, retry_backoff_s * 2)
+                time.sleep(retry_backoff_s)
+
+    def setup_exit_handler(self) -> None:
+        """Define exit conditions as application will typically run indefinitely"""
+        signal.signal(signal.SIGINT, self.exit_handler)
+        signal.signal(signal.SIGTERM, self.exit_handler)
+    
+    def exit_handler(self, _sig, _frame) -> None:
+        """Stop IoT/SPARK integration"""
+        print("Exit signal detected. Exiting...")
+        self.run_continuously = False
+        raise SignalException("Exit signal detected")
+            
+    def receive_spark_data(self, sock: socket.socket) -> (int, int):
+        """Receive SPARK data from producer socket"""
+        if sock is None: 
+            raise ConnectionError("SPARK producer socket not connected")
+
+        data = sock.recv(1024)
+        if not data:
+            raise ConnectionError("SPARK producer socket closing")
+
+        message = data.decode("utf-8").strip()
+        return tuple(map(int, message.split(",")))
 
     def device_callback(self, msg: Dict[str, Any]) -> None:
         print("\n--- Command Message Received in Firmware ---")
@@ -81,13 +115,13 @@ class IoTConnectClient:
         print("--- No further business logic implemented ---")
         print(json.dumps(msg))
 
-    def send_data(self) -> None:
+    def send_data(self, empty, taken) -> None:
         payload = [{
             "uniqueId": self.config['ids']['uniqueId'],
             "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000Z"),
             "data": {
-                "empty": random.randint(2, 8),
-                "taken": random.randint(1, 6),
+                "empty": empty,
+                "taken": taken,
                 "location": [49, 11]
             },
         }]
@@ -95,26 +129,31 @@ class IoTConnectClient:
         self.sdk.SendData(payload)
 
     def run_telemetry_continuously(self) -> None:
-        try:
-            with IoTConnectSDK(self.config['ids']['uniqueId'], self.config['ids']['sid'], self.sdk_options, self.device_connection_callback) as self.sdk:
-                self.device_list = self.sdk.Getdevice()
-                self.sdk.onDeviceCommand(self.device_callback)
-                self.sdk.onTwinChangeCommand(self.twin_update_callback)
-                self.sdk.onOTACommand(self.device_firmware_callback)
-                self.sdk.onDeviceChangeCommand(self.device_connection_callback)
-                self.sdk.getTwins()
-                self.device_list = self.sdk.Getdevice()
-                socket = self.connect_spark_socket()
-                while True:
-                    self.send_data()
-                    time.sleep(self.sdk_options['transmit_interval_seconds'])
-
-        except KeyboardInterrupt:
-            print("Keyboard Interrupt, exiting")
-            sys.exit(0)
-        except Exception as ex:
-            print(ex)
-            sys.exit(1)
+        """Run IoTConnect client continuously"""
+        max_retry_backoff_s = 300
+        retry_backoff_s = 5
+        while self.run_continuously:
+            try:
+                with IoTConnectSDK(self.config['ids']['uniqueId'], self.config['ids']['sid'], self.sdk_options, self.device_connection_callback) as self.sdk:
+                    self.device_list = self.sdk.Getdevice()
+                    self.sdk.onDeviceCommand(self.device_callback)
+                    self.sdk.onTwinChangeCommand(self.twin_update_callback)
+                    self.sdk.onOTACommand(self.device_firmware_callback)
+                    self.sdk.onDeviceChangeCommand(self.device_connection_callback)
+                    self.sdk.getTwins()
+                    self.device_list = self.sdk.Getdevice()
+                    spark_socket = self.connect_spark_socket()
+                    while True:
+                        empty, taken = self.receive_spark_data(spark_socket)
+                        self.send_data(empty, taken)
+                        time.sleep(self.sdk_options['transmit_interval_seconds'])
+            except SignalException:
+                sys.exit(0)
+            # exponential backoff
+            except Exception as ex:
+                print(ex)
+                time.sleep(retry_backoff_s)
+                retry_backoff_s = min(max_retry_backoff_s, retry_backoff_s * 2)
 
 if __name__ == "__main__":
     client = IoTConnectClient(['/opt/SPARK/iot/secrets.json', '/opt/SPARK/iot/config.json'])
