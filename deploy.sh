b3f064477a1f0984f47df791b2d07637930e9443
ljkeller <kellerjlucas@gmail.com>
2024-03-25 17:47:40 +0000

    IoT Connect Python Service
    Implemented service wrapping send_iot_connect.py. This service will
    launch when the network is up & restart as necessary. This service will
    block until there are datagram packets to catch.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <target>"
+    echo "Dont forget to enable your iotc-spark-server.service service"
     exit 1
 fi
 
@@ -13,6 +14,7 @@ TARGET=$1
     scp -r ./app/exe/ "root@${TARGET}:/home/root/dev/"
     ssh "root@${TARGET}" "mkdir -p /opt/spark/iot"
     scp ./app/iot/*.json "${TARGET}:/opt/spark/iot/"
+    scp ./app/iot/iotc-spark-server.service "${TARGET}:/etc/systemd/system/"
 
     set +ex
 )
--- install.sh
+++ install.sh
@@ -8,12 +8,16 @@ cp -r ./app/exe/* "${INSTALL_DIR}" || { echo "Failed to copy SPARK to RZBoard"; 
 cp ./runtime_deps/libtvm_runtime.so /usr/lib64/ || { echo "Failed to copy TVM runtime to RZboard"; exit 1;}
 cat ./app/weston_conf/weston.ini.append >> /etc/xdg/weston/weston.ini || { echo "Failed to append weston.ini"; exit 1;}
 
-if [ -f "/root/.config/weston.ini" ]; then
-    cat ./app/weston_conf/weston.ini.append >> /root/.config/weston.ini || { echo "Failed to append weston.ini to /root/.config/weston.ini"; exit 1;}
-else
-    mkdir -p "/root/.config" || { echo "Failed to create /root/.config directory"; exit 1;}
-    cp /etc/xdg/weston/weston.ini /root/.config/weston.ini || { echo "Failed to copy weston.ini to /root/.config/weston.ini"; exit 1;}
-fi
+# IoT Config
+mkdir -p ${INSTALL_DIR}/iot || { echo "Failed to create iot directory"; exit 1;}
+cp ./app/iot/*.json ${INSTALL_DIR}/iot/ || { echo "Failed to copy iot json files"; exit 1;}
+echo "Don't forget to update /opt/spark/iot/ config files with your own device keys/server orientation"
+
+# Service management
+cp ./app/iot/iotc-spark-server.service /etc/systemd/system/ || { echo "Failed to copy iotc-spark-server.service"; exit 1;}
+systemctl daemon-reload
+systemctl enable iotc-spark-server.service || { echo "Failed to enable iotc-spark-server.service"; exit 1;}
+systemctl start iotc-spark-server.service || { echo "Failed to start iotc-spark-server.service"; exit 1;}
 
 echo "SPARK installed successfully. Restarting Weston for changes to take effect."
 
