b81cd1519ffea5fc458ecdd46296538cfc9c7943
ljkeller <kellerjlucas@gmail.com>
2024-04-17 22:43:38 +0000

    Track occupancy duration in hrs, minutes

--- app/src/utils/ParkingSpot.cpp
+++ app/src/utils/ParkingSpot.cpp
@@ -10,6 +10,8 @@ ParkingSpot::ParkingSpot(size_t slot_id, cv::Rect coords, bool is_online, bool i
 
 void ParkingSpot::update_occupancy(bool is_occupied)
 {
+    // TODO: Should we update last_change_time on board initialization?
+    // Imagine when board is booted and a the slot is empty (the default value). Then, last_change_time isn't updated
     this->is_online = true;
     if (is_occupied != this->is_occupied)
     {
@@ -21,10 +23,18 @@ void ParkingSpot::update_occupancy(bool is_occupied)
 std::ostream &
 operator<<(std::ostream &os, const ParkingSpot &status)
 {
+    std::string duration_string = "";
+    if (status.last_change_time.has_value())
+    {
+        const auto hours = std::chrono::duration_cast<std::chrono::hours>(std::chrono::system_clock::now() - status.last_change_time.value());
+        const auto minutes = std::chrono::duration_cast<std::chrono::minutes>(std::chrono::system_clock::now() - status.last_change_time.value());
+        duration_string = std::to_string(hours.count()) + " hours " + std::to_string(minutes.count() % 60) + " minutes";
+    }
+
     os << "{"
        << "slot_id: " << status.slot_id << ", "
        << "is_occupied:" << status.is_occupied << ", "
-       << "last_change_time: " << (status.last_change_time.has_value() ? std::to_string(std::chrono::system_clock::to_time_t(status.last_change_time.value())) : "")
+       << "last_change_time: " << duration_string
        << "}";
     return os;
 }
